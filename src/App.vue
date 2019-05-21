385b8f1505c5c720c1958a47994da593a3c65b16
fudong <fudong515@qq.com>
2019-05-21 18:09:25 +0800

    Vue一般用法

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,15 @@
   <div id="app">
     <img src="./assets/logo.png">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <RuleDemo></RuleDemo>
     <AuthorComponent></AuthorComponent>
   </div>
 </template>
 
 <script>
-  import AuthorComponent from './components/AuthorComponent'  //引入组件
+  //引入组件
+  import AuthorComponent from './components/AuthorComponent'
+  import RuleDemo from './components/RuleDemo'
   export default {
     name: 'app',
     data () {
@@ -29,7 +18,7 @@
         msg: 'Welcome to Your Vue App'
       }
     },
-    components:{ AuthorComponent }  //组件注册
+    components:{ AuthorComponent, RuleDemo }  //注册组件
   }
 </script>
 
---
+++ src/components/RuleDemo.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="RuleDemo">
+  <!-- 指令带有前缀 v-，以表示它们是Vue提供的特殊特性 -->
+  <span v-bind:title="message">
+    鼠标悬停几秒钟查看此处动态绑定的提示信息！
+  </span>
+  <!-- 是否展示 -->
+  <p v-if="seen">现在你看到我了</p>
+  <!-- list型数据遍历 -->
+  <ol>
+    <li v-for="r in results">
+      {{ r.text }}
+    </li>
+  </ol>
+  <!-- 按钮绑定方法 -->
+  <p>{{ egMsg }}</p>
+  <button v-on:click="reverseMsg">逆转上边的消息</button>
+  <!-- 数据的双向绑定 -->
+    <label>
+      数据的双向绑定:
+      <input v-model="egMsg">
+    </label>
+  </div>
+</template>
+
+<script>
+    export default {
+      data() {
+        return {
+          egMsg: '123456',
+          message: '页面加载于 ' + new Date().toLocaleString(),
+          seen: true,
+          results: [
+            {text: 'data1'},
+            {text: 'data2'},
+            {text: 'data3'}
+          ]
+        }
+      },
+      methods: {
+        //逆转字符串
+        reverseMsg: function () {
+          this.egMsg = this.egMsg.split('').reverse().join('')
+        }
+      }
+    }
+</script>
+
+<style>
+  label {
+    display: block;
+  }
+</style>
