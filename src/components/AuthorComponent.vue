8b8bc8a079ef0e28ee5cbe6243554043e6279559
fudong <fudong515@qq.com>
2019-05-21 16:03:41 +0800

    Init

---
+++ src/components/AuthorComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <h3>Author:{{name}}</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+      data (){
+        return {
+          name: "Sag"
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
